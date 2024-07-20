5db51b0f83c512ce8374437fed1d395e91b8f39c
Sacvertello <georgdvorskii@gmail.com>
2024-07-20 17:36:55 +0300

    Rework homework 14

--- assets/js/js-code.js
+++ assets/js/js-code.js
@@ -8,41 +8,46 @@ if ((number1 + number2) % 5 === 0) {
 }
 //2
 const sideOfSquare = Number(prompt('Введіть сторону квадрата:'))
-const areaOfSquare = sideOfSquare*sideOfSquare
-console.log("Площа квадрата:", areaOfSquare)
+function areaOfSquare (){
+    return sideOfSquare**2
+}
+const resultOfArea = areaOfSquare();
+console.log(resultOfArea)
 //3
 const menu = "Ознайомтесь з меню: \n 1.Чай \n 2.Кава \n 3.Сік \n 4.Вода"
 alert(menu)
 const order = Number(prompt('Введіть цифру напою який хочете:'))
-if (order===1) {
-    alert('Ви вибрали чай!')
-} else if (order===2) {
-    alert('Ви вибрали каву!')
-}  else if (order===3) {
-    alert('Ви вибрали сік!')
-} else if (order===4) {
-    alert('Ви вибрали воду!')
-}  else {
-    alert('Вибачте, але ми такого напою не маємо(')
+switch (order) {
+    case 1: 
+        alert('Ви вибрали чай!');
+        break;
+    case 2:
+        alert('Ви вибрали каву!');
+        break;
+    case 3:
+        alert('Ви вибрали сік!');
+        break;
+    case 4:
+        alert('Ви вибрали воду!');
+        break;
+    default:
+        alert('Вибачте, але ми такого напою не маємо(')
 }
 //4
 const sumOfPurchase =Number(prompt("Введіть суму покупки"))
 const discountOf5percent = sumOfPurchase - sumOfPurchase/100*5
 const discountOf3percent = sumOfPurchase - sumOfPurchase/100*3
-if (sumOfPurchase>500) {
-    if (sumOfPurchase>800){
-        console.log("Вітаю! вам надана знижка 5%. Сума покупки: ", discountOf5percent)
-    } else{
-        console.log("Вітаю! вам надана знижка 3%. Сума покупки: ", discountOf3percent)
-    }
-} else {
-    console.log("Сума покупки:", sumOfPurchase )
-}
+
+let purchase = (sumOfPurchase <= 500) ? `Сума покупки: ${sumOfPurchase}`:
+    (sumOfPurchase>500) ? `Вітаю! вам надана знижка 3%. Сума покупки:  ${discountOf3percent}`: 
+    (sumOfPurchase> 800) ? `Вітаю! вам надана знижка 5%. Сума покупки: ${discountOf5percent}`:
+    '.';
+console.log(purchase);
 //5
 const digit1 = Number(prompt("Введіть ділене:"))
 const digit2 = Number(prompt("Введіть дільник:"))
 if (digit1 % digit2 === 0) {
-    alert(Boolean(true))
+    alert(true)
 } else {
-    alert(Boolean(false))
+    alert(false)
 }
